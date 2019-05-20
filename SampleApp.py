fcca4f1e1216fcaafc5b3aba62b9c8fc69dd7a67
Felix <felix@workstation.speedport.ip>
2019-05-20 19:02:48 +0200

    Extended samples

--- SampleApp.py
+++ SampleApp.py
@@ -8,6 +8,9 @@ headCamImage = cv2.imread('./images/tray_resistor.png')
 bedCamImage = cv2.imread('./images/tiny-on-gripper.png')
 templateImage = cv2.imread('./images/template-output.png')
 
+
+#---------<
+# Scenario 1 - Find position of object in tray picture
 #---------<
 # Read the HSV color range from a background image
 maskValues = VisionPNP.getHSVColorRange('./images/gripper.png')
@@ -28,8 +31,26 @@ croppedImageConv = np.array(croppedImage)
 position = VisionPNP.findShape(croppedImageConv)
 print(position)
 cv2.circle(croppedImageConv,(position[0], position[1]), 4, (0,0,255), -1)
-cv2.imwrite('./01_object_position.png', croppedImageConv)
+cv2.imwrite('./01_object_com_tray.png', croppedImageConv)
 
+#---------<
+# Scenario 2 - Find position of object in gripper image (no orientation)
+#---------<
+# Create working copy
+bedCamImageCopy = bedCamImage.copy()
+# Clean green background
+cleanedBedCamRaw = VisionPNP.removeColorRange(bedCamImageCopy, maskValues)
+cleanedBedCam = np.array(cleanedBedCamRaw)
+
+# Find center of mass
+center = VisionPNP.findShape(cleanedBedCam)
+print(center)
+cv2.circle(bedCamImageCopy,(center[0], center[1]), 4, (0,0,255), -1)
+cv2.imwrite('./02_object_com_gripper.png', bedCamImageCopy)
+
+
+#---------<
+# Scenario 3 - Find template in search image (with orientation)
 #---------<
 # Find a binary template inside a provided input image.
 # The maskValues contain the color range of the background color for easier seperation.

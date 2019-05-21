9851787a6d455cabd219805fcc66e7f59fca393d
platsch <wasserfall@kalanka.de>
2019-05-21 07:55:48 +0000

    Modified for odroid compatability

--- src/python_module.cpp
+++ src/python_module.cpp
@@ -30,72 +30,6 @@ string type2str(int type) {
   return r;
 }
 
-
-// Mat (int ndims, const int *sizes, int type, void *data, const size_t *steps=0)
-// Mat (long int&, cv::Size*, int, void*&, __gnu_cxx::__alloc_traits<std::allocator<long int>, long int>::value_type*)’
-
-
-
-// static Mat numpyToMat(py::array imgArray) {
-//   auto info = imgArray.request();
-//   decltype(CV_32F) dtype;
-//   if(info.format == py::format_descriptor<float>::value) dtype = CV_32F;
-//   else if (info.format == py::format_descriptor<double>::value) dtype = CV_64F;
-
-
-//   auto ndims = info.ndim;
-//   Size shape (info.shape[0], info.shape[1]);
-//   auto& strides = info.strides;
-//   Mat image(int(&ndims), &shape, CV_32F, info.ptr, &strides[0]);
-//   return image;
-// }
-
-
-    // bool load(handle src, bool) {
-    //     array b(src, true);
-    //     if(!b.check()) return false;
-    //     auto info = b.request();
-
-    //     decltype(CV_32F) dtype;
-    //     if(info.format == format_descriptor<float>::value) dtype = CV_32F;
-    //     else if (info.format == format_descriptor<double>::value) dtype = CV_64F;
-    //     else return false;
-
-    //     auto ndims = info.ndim;
-    //     auto shape = std::vector<int>(info.shape.begin(), info.shape.end());
-    //     auto& strides = info.strides;
-    //     value = cv::Mat(ndims,
-    //             &shape[0],
-    //             dtype,
-    //             info.ptr,
-    //             &strides[0]);
-    //     return true;
-
-    // }
-
-    // static handle cast(const cv::Mat &m, return_value_policy, handle defval) {
-    //     auto format = format_descriptor<float>::value;
-    //     auto type = m.type();
-    //     switch(type) {
-    //         case CV_32F: format = format_descriptor<float>::value; break;
-    //         case CV_64F: format = format_descriptor<double>::value; break;
-    //         default: return defval;
-    //     }
-
-    //     std::vector<size_t> IHateBjarneStroustrup;
-    //     std::copy(m.size.p, m.size.p + m.dims, std::back_inserter(IHateBjarneStroustrup));
-    //     auto strides = std::vector<size_t>(m.step.p, m.step.p + m.dims);
-    //     strides.push_back(1);
-    //     return array(buffer_info(
-    //         m.data,
-    //         m.elemSize1(),
-    //         format,
-    //         m.dims,
-    //         IHateBjarneStroustrup,
-    //         strides
-    //         )).release();
-    // }
-
 //----------------------------------------------------------<
 // numpy array to Mat converter
 static Mat numpyToMat(py::array input) {
@@ -172,8 +106,9 @@ static Mat pyCropImageToRect(const py::array& inputImage, const vector<int> boud
 }
 //----------------------------------------------------------<
 // Python module definition
-PYBIND11_MODULE(VisionPNP, m){
-  m.doc() = "Tool for detecting objects in images.";
+
+PYBIND11_PLUGIN(VisionPNP){
+  py::module m("VisionPNP", "python plugin for cv pick and place automation");
 
   m.def("numpyToMat", &numpyToMat);
 
@@ -244,4 +179,5 @@ PYBIND11_MODULE(VisionPNP, m){
         }
       );
     });
+  return m.ptr();
 }
